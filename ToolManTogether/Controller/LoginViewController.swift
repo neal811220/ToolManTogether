6d57f68691c00f09e852e44acaa69deb4dbb7461
Spoke <spoke@Spokede-MacBook-Air.local>
2018-09-19 10:40:32 +0800

    Create ignore file and install SwiftLint

---
+++ ToolManTogether/Controller/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  ToolManTogether
+//
+//  Created by Spoke on 2018/9/19.
+//  Copyright © 2018年 Spoke. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+
+
+}
